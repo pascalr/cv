3d32a02ed9cbe8eda351e190894efc31612ca86e
Dark Krael <dark.krael@gmail.com>
2022-05-23 08:51:26 -0400

    .

---
+++ app/javascript/application.js
@@ -0,0 +1,3 @@
+// I don't know how this works but this works well.
+// It will bind js to all elements with attribute data-bs-toggle"..."
+import * as bootstrap from 'bootstrap' // OPTIMIZE: Don't import everything?
