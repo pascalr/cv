f2666c982ddcd7934dcc1d16e27a11548a2aab1b
Dark Krael <dark.krael@gmail.com>
2022-09-03 07:51:55 -0400

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,6 +47,16 @@ module ApplicationHelper
       navbar_projects: 'Projects',
       navbar_trips: 'Trips',
       welcome_message: '<b>Hello</b>, I am Pascal, a self-taught <b>programmer</b> with varied interests, like ' + (link_to "programming", prog_path) + ', ' + (link_to "robotics", robot_path) + ', ' + (link_to "travel", trips_path) + ', ' + (link_to "mechanical conception", conception_path) + ' and guitar!',
+      job_title_tld: 'MECHANICAL ENGINEERING INTERN',
+      job_desc_tld_1: 'Design new parts and assemblies for a machine prototype',
+      job_desc_tld_2: 'Cooperate with production to clarify installation instructions',
+      job_desc_tld_3: 'Draw and modify mechanical plans',
+      job_name_farm: 'FAMILY FARM',
+      job_title_farm: 'FARMER',
+      job_desc_farm_1: 'Repair and maintain farming equipment',
+      job_desc_farm_2: 'Build farm buildings integrally',
+      job_desc_farm_3: 'Drive tractors and operate farming machinery',
+
     }
 
     # FRENCH
@@ -57,6 +67,15 @@ module ApplicationHelper
       navbar_projects: 'Projets',
       navbar_trips: 'Voyages',
       welcome_message: "<b>Salut</b>, je m'appelle Pascal. Je suis un <b>programmeur</b> autodidacte avec de multiples intérêts, comme la " + (link_to "programmation", prog_path) + ', la ' + (link_to "robotique", robot_path) + ', le ' + (link_to "voyage", trips_path) + ', la ' + (link_to "conception mécanique", conception_path) + ' et la guitare!',
+      job_title_tld: 'STAGIAIRE EN GÉNIE MÉCANIQUE',
+      job_desc_tld_1: 'Concevoir de nouvelles pièces et assemblages pour une machine prototype',
+      job_desc_tld_2: 'Coopérer avec la production pour clarifier les instructions d’installations',
+      job_desc_tld_3: 'Générer et modifier des plans mécaniques',
+      job_name_farm: 'FERME FAMILIALE',
+      job_title_farm: 'AGRICULTEUR',
+      job_desc_farm_1: 'Réparer et entretenir des équipements agricoles',
+      job_desc_farm_2: 'Construire des bâtiments de ferme intégralement',
+      job_desc_farm_3: 'Conduire des tracteurs et opérer de la machinerie',
     }
 
     text = in_english? ? english[key.to_sym] : french[key.to_sym]
