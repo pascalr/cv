1f5685bd486d22d4ec2ff6486407dd8ae1b3fe46
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-29 10:52:28 -0500

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,15 @@ module ApplicationHelper
     return get_locale == :fr
   end
 
+  def link_with_icon_to(label, link)
+    link_to link do
+      (label + image_tag('link.svg', style: 'margin: 0 0 0.2em 0.1em')).html_safe
+      #content_tag('span', class: 'd-inline-flex align-items-center') do
+      #  (content_tag('span', label) + image_tag('link.svg')).html_safe
+      #end
+    end
+  end
+
   def timeline_item(date, title, item=nil)
     @is_left = true unless defined?(@is_left)
     r = ''
@@ -75,7 +84,7 @@ module ApplicationHelper
       navbar_trips: 'Trips',
       navbar_about: 'About',
       navbar_contact: 'Contact',
-      welcome_message: '<b>Hello</b>, I am Pascal, a self-taught <b>programmer</b> with varied interests, like ' + (link_to "programming", prog_path) + ', ' + (link_to "robotics", robot_path) + ', ' + (link_to "travel", trips_path) + ', ' + (link_to "mechanical conception", conception_path) + ' and guitar!',
+      welcome_message: '<b>Hello</b>, I am Pascal, a self-taught <b>full-stack developer</b> with varied interests, like ' + (link_with_icon_to "programming", prog_path) + ', ' + (link_with_icon_to "robotics", robot_path) + ', ' + (link_with_icon_to "travel", trips_path) + ', ' + (link_with_icon_to "mechanical conception", conception_path) + ' and guitar!',
       job_title_tld: 'MECHANICAL ENGINEERING INTERN',
       job_desc_tld_1: 'Design new parts and assemblies for a machine prototype',
       job_desc_tld_2: 'Cooperate with production to clarify installation instructions',
@@ -94,8 +103,9 @@ module ApplicationHelper
       job_desc_ppd_1: 'Modify programming logic controllers code (Ladder)',
       job_desc_ppd_2: 'Manipulate and configure an industrial Kuka robot',
       job_desc_ppd_3: 'Fix electrical, mechanical and hydraulic systems',
+      exp_node: 'Programming back-end in javascript with multiple librairies like Express.js.',
       exp_rails: 'Programming back-end RESTful, handling databases, MVC framework.',
-      exp_react: 'Programming UI in javascript, with vanilla javascript and React.js library.',
+      exp_react: 'Programming UI in javascript with React.js or vanilla javascript.',
       exp_css: 'Design front end with CSS and HTML. Familiar with Bootstrap library.',
       exp_cpp: 'Programming low level C/C++. Coding for Arduino microcontroller and video game conception.',
       exp_godot: 'Programming a robotic arm simulation with Godot software.',
@@ -116,7 +126,7 @@ module ApplicationHelper
       navbar_trips: 'Voyages',
       navbar_about: 'À propos',
       navbar_contact: 'Me contacter',
-      welcome_message: "<b>Salut</b>, je m'appelle Pascal. Je suis un <b>programmeur</b> autodidacte avec de multiples intérêts, comme la " + (link_to "programmation", prog_path) + ', la ' + (link_to "robotique", robot_path) + ', le ' + (link_to "voyage", trips_path) + ', la ' + (link_to "conception mécanique", conception_path) + ' et la guitare!',
+      welcome_message: "<b>Salut</b>, je m'appelle Pascal. Je suis un <b>programmeur full-stack</b> autodidacte avec multiples intérêts, comme la " + (link_with_icon_to "programmation", prog_path) + ', la ' + (link_with_icon_to "robotique", robot_path) + ', le ' + (link_with_icon_to "voyage", trips_path) + ', la ' + (link_with_icon_to "conception mécanique", conception_path) + ' et la guitare!',
       job_title_tld: 'STAGIAIRE EN GÉNIE MÉCANIQUE',
       job_desc_tld_1: 'Concevoir de nouvelles pièces et assemblages pour une machine prototype',
       job_desc_tld_2: 'Coopérer avec la production pour clarifier les instructions d’installations',
@@ -135,8 +145,9 @@ module ApplicationHelper
       job_desc_ppd_1: 'Modifier la programmation d’automates programmables (Ladder)',
       job_desc_ppd_2: 'Manipuler et configurer un robot industriel Kuka',
       job_desc_ppd_3: 'Réparer des systèmes électriques, mécaniques et hydrauliques',
+      exp_node: 'Programmation back-end en javascript avec de multiples librairies comme Express.js.',
       exp_rails: 'Programmation back-end RESTful, gestion de base de données, framework MVC.',
-      exp_react: 'Programmation de UI en javascript, en simple javascript et avec la librairie React.js.',
+      exp_react: 'Programmation de UI en javascript avec React.js ou en en simple javascript.',
       exp_css: 'Design front end avec CSS et HTML. Familiarité avec la librairie Bootstrap.',
       exp_cpp: 'Programmation de bas niveau en C/C++. Code pour microcontrôleur Arduino et conception de jeux vidéo.',
       exp_godot: "Programmation d'une simulation de bras robotique avec le logiciel Godot.",
